796452c5acee440d74eb2736d1ec737b5b5105d5
tluettje <tluettje@techfak.uni-bielefeld.de>
2019-03-08 12:59:02 +0100

    finalize helper script

--- bin/project_usage-compose.py
+++ bin/project_usage-compose.py
@@ -21,11 +21,17 @@ __license__ = "MIT"
 docker_compose_path = which("docker-compose")
 project_dir = Path(__file__).parent.parent
 
+shared_env_file = {project_dir / ".shared.default.env": project_dir / ".shared.env"}
 # mapping between default/example files and files with customized values
 env_files = {
-    project_dir / ".portal.default.env": project_dir / ".portal.env",
-    project_dir / ".shared.default.env": project_dir / ".shared.env",
-    project_dir / ".site.default.env": project_dir / ".site.env",
+    "portal": {
+        project_dir / ".portal.default.env": project_dir / ".portal.env",
+        **shared_env_file,
+    },
+    "site": {
+        project_dir / ".site.default.env": project_dir / ".site.env",
+        **shared_env_file,
+    },
 }
 
 development_files = [
@@ -34,16 +40,25 @@ development_files = [
     project_dir / "dev" / "docker-compose.override.yml",
 ]
 
+prod_files = {
+    "portal": [
+        project_dir / "docker-compose.portal.yml",
+        project_dir / "prod" / "docker-compose.portal.override.yml",
+    ],
+    "site": [
+        project_dir / "docker-compose.site.yml",
+        project_dir / "prod" / "docker-compose.site.override.yml",
+    ],
+}
 
-def parse_env_files() -> Dict[str, str]:
+
+def parse_env_files(env_files: Dict[Path, Path]) -> Dict[str, str]:
     env = {}
     for default_file, custom_file in env_files.items():
         env_file = default_file
         if custom_file.exists():
             logging.debug(
-                "Reading %s instead of default file %s".format(
-                    custom_file, default_file
-                )
+                "Reading %s instead of default file %s", custom_file, default_file
             )
             env_file = custom_file
         else:
@@ -63,6 +78,13 @@ def parse_env_files() -> Dict[str, str]:
     return env
 
 
+def format_env(env: Dict[str, str], separator: str = "\n") -> str:
+    return separator.join(
+        "{key}={value}".format(key=key, value="'{}'".format(value) if value else "")
+        for key, value in env.items()
+    )
+
+
 def main() -> int:
 
     parser = ArgumentParser(
@@ -75,10 +97,13 @@ def main() -> int:
     )
 
     subparsers = parser.add_subparsers(
-        title="Subcommands", description="Different launch modi", dest="subcommand"
+        title="Subcommands",
+        description="Different launch modi",
+        dest="subcommand",
+        required=True,
     )
 
-    dev_parser = subparsers.add_parser(
+    subparsers.add_parser(
         "development",
         aliases=["dev"],
         description="""Run the whole `project_usage` in development mode. Usage data
@@ -88,16 +113,30 @@ def main() -> int:
         service. All Prometheus and Grafana instances are launched with port mappings.""",
     )
 
-    print_env_parser = subparsers.add_parser(
-        "print-env",
-        aliases=["env"],
-        description="""Print the parsed environment variables, can be used to export it
-        into the current shell by `export $({} print-env | xargs)""".format(
+    prod_parser = subparsers.add_parser(
+        "production",
+        aliases=["prod"],
+        description="""Run either the `site` or the `portal` stack in production mode.""",
+    )
+    prod_parser.add_argument("stack", choices=("portal", "site"))
+
+    non_docker_actions = parser.add_mutually_exclusive_group()
+
+    non_docker_actions.add_argument(
+        "-p",
+        "--print-env",
+        action="store_true",
+        help="""Print the parsed environment variables separated by newline, can be
+        used to export i tinto the current shell by `export $({} print-env |
+        xargs)""".format(
             argv[0]
         ),
     )
-    print_env_parser.add_argument(
-        "-s", "--separator", default="\n", help="Separator between `key=value` pairs"
+    non_docker_actions.add_argument(
+        "-n",
+        "--dry-run",
+        action="store_true",
+        help="Do not exec docker-compose, only print call.",
     )
 
     args, remaining_args = parser.parse_known_args()
@@ -111,25 +150,27 @@ def main() -> int:
     if not docker_compose_path:
         logging.critical("Could not detect `docker-compose` in $PATH. Exiting")
         return 1
+    call = [docker_compose_path]
 
-    if "env" in args.subcommand:
-        print(
-            args.separator.join(
-                "{key}={value}".format(
-                    key=key, value="'{}'".format(value) if value else ""
-                )
-                for key, value in parse_env_files().items()
-            )
-        )
-        return 0
     if "dev" in args.subcommand:
-        call = [docker_compose_path]
-        for file in development_files:
-            call.extend(("-f", str(file)))
-        call.extend(remaining_args)
-        env = parse_env_files()
-        logging.debug("Executing %s with environment %s", call, env)
-        execve(docker_compose_path, call, env)
+        needed_compose_files = development_files
+        needed_env_files = {**env_files["portal"], **env_files["site"]}
+    else:
+        needed_compose_files = prod_files[args.stack]
+        needed_env_files = env_files[args.stack]
+
+    env = parse_env_files(needed_env_files)
+    if args.print_env:
+        print(format_env(env))
+        return 0
+    for file in needed_compose_files:
+        call.extend(("--file", str(file)))
+    call.extend(remaining_args)
+    if args.dry_run:
+        print(" ".join(call))
+        return 0
+    logging.debug("Executing %s with environment %s", call, env)
+    execve(docker_compose_path, call, env)
 
     return 0
 
