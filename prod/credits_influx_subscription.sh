0f06accd85cde63661daa03fad125fdbd223e054
tluettje <tluettje@techfak.uni-bielefeld.de>
2019-04-17 14:40:38 +0200

    prepare db for credits_history inside influxdb

--- prod/credits_influx_subscription.sh
+++ prod/credits_influx_subscription.sh
@@ -2,5 +2,10 @@
 
 influx -username "$INFLUXDB_ADMIN_USER" \
 	-password "$INFLUXDB_ADMIN_PASSWORD" \
-	-database "$INFLUXDB_DB" \
 	-execute "create subscription \"credits\" on \"$INFLUXDB_DB\".\"autogen\" destinations all 'http://portal_credits:80'"
+influx -username "$INFLUXDB_ADMIN_USER" \
+	-password "$INFLUXDB_ADMIN_PASSWORD" \
+	-execute "create database \"$CREDITS_HISTORY_DB\""
+influx -username "$INFLUXDB_ADMIN_USER" \
+	-password "$INFLUXDB_ADMIN_PASSWORD" \
+	-execute "grant all on \"$CREDITS_HISTORY_DB\" to \"$INFLUXDB_USER\""
